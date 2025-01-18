dc474dc4aeb549d301302e0fab6e518ee3bea1e1
Alexandr Gromov <gromovaleksandr813@gmail.com>
2025-01-18 19:17:44 +0300

    Initial

---
+++ home_work_12_PTR_3/home_work_12_PTR_3.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <iostream>
+template<class T>
+class unique_ptr {
+public:
+    unique_ptr(T* p) : ptr{ p } {}
+
+    ~unique_ptr() // деструктор
+    {
+        if (ptr) {
+            std::cout << "destructor called\n";
+            delete[] ptr;
+        }
+    }
+
+    T* release() {
+        T* p = ptr;
+        ptr = nullptr;
+        return p;
+    }
+    unique_ptr (unique_ptr& other) = delete;
+
+
+    T& operator*(){
+        if (ptr)
+        return  *ptr;
+   }
+
+   T& operator=(const unique_ptr& other) = delete;
+
+    private:
+    T* ptr = new T{ nullptr };
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    unique_ptr<int> ptr(new int(17));
+    unique_ptr<int> ptr_2(new int(10));
+
+    std::cout << *ptr << std::endl;
+    std::cout << *ptr_2 << std::endl;
+   // ptr = ptr_2;
+   // unique_ptr<int> ptr_3(ptr);
+    int* ptr_4 = ptr.release();
+    std::cout << *ptr_4 << std::endl;
+    std::cout << *ptr << std::endl;
+    return 0;
+}
+
---
+++ home_work_12_Ptr_1/home_work_12_Ptr_1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+
+class tridiagonal_matrix
+{
+public:
+    std::vector<double> m_down;
+    std::vector<double> m_upper;
+    std::vector<double> m_middle;
+    tridiagonal_matrix(
+        const std::vector<double>& down,
+        const std::vector<double>& upper,
+        const std::vector<double>& middle) :
+        m_down{ down }, m_upper{ upper }, m_middle{ middle }
+    {};
+    ~tridiagonal_matrix() { std::cout << "destructor called\n"; }
+
+    std::unique_ptr<tridiagonal_matrix> clone() {
+       std::unique_ptr<tridiagonal_matrix> ptr(new tridiagonal_matrix(m_down, m_upper, m_middle));
+       return ptr;
+    };
+};
+
+int main()
+{
+    auto down = std::vector<double>{ -4.0, 5.0 };
+    auto upper = std::vector<double>{ 14.0, 8.0 };
+    auto middle = std::vector<double>{ 3.0, 1.0, 7.0 };
+    auto matrix = std::make_unique<tridiagonal_matrix>(
+        down,
+        upper,
+        middle
+    );
+
+    auto matrix_clone = matrix->clone();
+
+    for (auto i : matrix->m_down) {
+        std::cout << i << std::endl;
+    }
+    for (auto i : matrix->m_upper) {
+        std::cout << i << std::endl;
+    }
+    for (auto i : matrix->m_middle) {
+        std::cout << i << std::endl;
+    }
+    for (auto i: matrix_clone->m_down){
+        std::cout << i << std::endl;
+    }
+    for (auto i : matrix_clone->m_upper) {
+        std::cout << i << std::endl;
+    }
+    for (auto i : matrix_clone->m_middle) {
+        std::cout << i << std::endl;
+    }
+    return 0;
+}
